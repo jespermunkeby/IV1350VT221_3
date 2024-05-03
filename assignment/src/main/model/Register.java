56d2208d89bed288c8eb7cac3710c456c0af8761
Jesper Munkeby <46975845+jespermunkeby@users.noreply.github.com>
2024-05-03 08:46:43 +0200

    Update Register.java

--- assignment/src/main/model/Register.java
+++ assignment/src/main/model/Register.java
@@ -10,10 +10,20 @@ public class Register {
        return this.balance;
    }
 
+   /**
+   * Adds the specified amount to the current balance.
+   * @param amount The amount to deposit. Must be a non-negative number.
+   */
    public void depopsitToBalance(float amount){
        this.balance += amount;
    }
 
+   /**
+  * Prints a formatted receipt with details from a sale and its corresponding payment.
+  * 
+  * @param saleInfo The details of the sale.
+  * @param paymentDetails The details of the payment.
+  */
    public void printReceipt(SaleInfoDTO saleInfo, PaymentDetailsDTO paymentDetails){
         System.out.println("\nRECEIPT:");
         System.out.println(saleInfo);
