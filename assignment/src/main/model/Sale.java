5cbab9ed289485222e062fcd6e34afc65bd2799a
Jesper Munkeby <46975845+jespermunkeby@users.noreply.github.com>
2024-05-03 08:49:32 +0200

    Update Sale.java

--- assignment/src/main/model/Sale.java
+++ assignment/src/main/model/Sale.java
@@ -11,12 +11,24 @@ public class Sale {
     private float discount;
     private Integration integration;
 
+    /**
+     * Constructs a new Sale instance with a reference to an integration layer.
+     * 
+     * @param integration The integration system to be used for database or external service access.
+     */
     public Sale(Integration integration){
         this.items = new HashMap<Integer, ItemDTO>();
         this.discount = 0;
         this.integration = integration;
     }
 
+     /**
+     * Attempts to add an item to the sale by its ID and quantity.
+     * 
+     * @param id The ID of the item to be added.
+     * @param quantity The quantity of the item to be added.
+     * @return true if the item is added successfully, false if the item cannot be found.
+     */
     public boolean addItemByID(int id, int quantity){
         ItemDTO item = this.integration.getItemDTOByID(id, quantity);
 
@@ -39,10 +51,21 @@ public class Sale {
 
     // }
 
+    /**
+     * Finalizes the sale and returns the sale information.
+     * 
+     * @param paymentDetails The payment details for the sale.
+     * @return SaleInfoDTO object containing details about the sale.
+     */
     public SaleInfoDTO finishSale(PaymentDetailsDTO paymentDetails){
         return this.getSaleInfo();
     }
 
+    /**
+     * Retrieves the current sale information.
+     * 
+     * @return SaleInfoDTO containing the items and discount applied to the sale.
+     */
     public SaleInfoDTO getSaleInfo(){
         return new SaleInfoDTO(this.items.values(), this.discount);
     }
